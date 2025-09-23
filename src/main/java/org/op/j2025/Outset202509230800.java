325551eeb112f52d16221653e3783fb9d608af60
xx008888 <xx008888@outlook.com>
2025-09-23 16:00:14 +0800

    add Outset202509230800

---
+++ src/main/java/org/op/j2025/Outset202509230800.java
@@ -0,0 +1,17 @@
+package org.op.j2025;
+
+public class Outset202509230800 {
+
+    public String o0; 
+
+    public String u1; 
+
+    public String t2; 
+
+    public String s3; 
+
+    public String e4; 
+
+    public String t5; 
+
+}
