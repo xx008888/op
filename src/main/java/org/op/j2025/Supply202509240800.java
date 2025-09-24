e0fc6284b7d72c577d44dbd4f613514b8e0c7d40
xx008888 <xx008888@outlook.com>
2025-09-24 16:00:57 +0800

    add Supply202509240800

---
+++ src/main/java/org/op/j2025/Supply202509240800.java
@@ -0,0 +1,17 @@
+package org.op.j2025;
+
+public class Supply202509240800 {
+
+    public String s0; 
+
+    public String u1; 
+
+    public String p2; 
+
+    public String p3; 
+
+    public String l4; 
+
+    public String y5; 
+
+}
