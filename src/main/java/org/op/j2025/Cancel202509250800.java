d0fbfd682f8f40d4b4827bfc70fcf88de5b64520
xx008888 <xx008888@outlook.com>
2025-09-25 16:00:28 +0800

    add Cancel202509250800

---
+++ src/main/java/org/op/j2025/Cancel202509250800.java
@@ -0,0 +1,17 @@
+package org.op.j2025;
+
+public class Cancel202509250800 {
+
+    public String c0; 
+
+    public String a1; 
+
+    public String n2; 
+
+    public String c3; 
+
+    public String e4; 
+
+    public String l5; 
+
+}
