fb5615377d65eb2944b396e14b0f854f08acc5ea
xx008888 <xx008888@outlook.com>
2025-09-27 16:00:48 +0800

    add Occur202509270800

---
+++ src/main/java/org/op/j2025/Occur202509270800.java
@@ -0,0 +1,15 @@
+package org.op.j2025;
+
+public class Occur202509270800 {
+
+    public String o0; 
+
+    public String c1; 
+
+    public String c2; 
+
+    public String u3; 
+
+    public String r4; 
+
+}
