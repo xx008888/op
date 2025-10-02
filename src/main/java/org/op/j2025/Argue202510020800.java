6173a3a8440605fe51f7bf93fbc5fcae7cdbe8d9
xx008888 <xx008888@outlook.com>
2025-10-02 16:00:24 +0800

    add Argue202510020800

---
+++ src/main/java/org/op/j2025/Argue202510020800.java
@@ -0,0 +1,15 @@
+package org.op.j2025;
+
+public class Argue202510020800 {
+
+    public String a0; 
+
+    public String r1; 
+
+    public String g2; 
+
+    public String u3; 
+
+    public String e4; 
+
+}
