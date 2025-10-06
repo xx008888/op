13adfeb4e2d559c51948abdcd3857f5309a577bc
xx008888 <xx008888@outlook.com>
2025-10-07 00:01:00 +0800

    add Cavity202510061600

---
+++ src/main/java/org/op/j2025/Cavity202510061600.java
@@ -0,0 +1,17 @@
+package org.op.j2025;
+
+public class Cavity202510061600 {
+
+    public String c0; 
+
+    public String a1; 
+
+    public String v2; 
+
+    public String i3; 
+
+    public String t4; 
+
+    public String y5; 
+
+}
