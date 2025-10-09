9bd55d9748c8a340f7e3604f8ab1ce62ecabf4ac
xx008888 <xx008888@outlook.com>
2025-10-09 16:00:41 +0800

    add Rocket202510090800

---
+++ src/main/java/org/op/j2025/Rocket202510090800.java
@@ -0,0 +1,17 @@
+package org.op.j2025;
+
+public class Rocket202510090800 {
+
+    public String r0; 
+
+    public String o1; 
+
+    public String c2; 
+
+    public String k3; 
+
+    public String e4; 
+
+    public String t5; 
+
+}
