e9a3dc70501586af59c6c8141ee45b1b2f6ec230
xx008888 <xx008888@outlook.com>
2025-10-10 16:00:14 +0800

    add Affirm202510100800

---
+++ src/main/java/org/op/j2025/Affirm202510100800.java
@@ -0,0 +1,17 @@
+package org.op.j2025;
+
+public class Affirm202510100800 {
+
+    public String a0; 
+
+    public String f1; 
+
+    public String f2; 
+
+    public String i3; 
+
+    public String r4; 
+
+    public String m5; 
+
+}
