e450d7ddec2717046184038cce4605fabe029c7e
xx008888 <xx008888@outlook.com>
2025-10-13 16:00:40 +0800

    add Reserve202510130800

---
+++ src/main/java/org/op/j2025/Reserve202510130800.java
@@ -0,0 +1,19 @@
+package org.op.j2025;
+
+public class Reserve202510130800 {
+
+    public String r0; 
+
+    public String e1; 
+
+    public String s2; 
+
+    public String e3; 
+
+    public String r4; 
+
+    public String v5; 
+
+    public String e6; 
+
+}
