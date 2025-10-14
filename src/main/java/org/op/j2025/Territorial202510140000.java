a815aeb9857c72e3be432032eb690fa6e0cffc7e
xx008888 <xx008888@outlook.com>
2025-10-14 08:00:22 +0800

    add Territorial202510140000

---
+++ src/main/java/org/op/j2025/Territorial202510140000.java
@@ -0,0 +1,27 @@
+package org.op.j2025;
+
+public class Territorial202510140000 {
+
+    public String t0; 
+
+    public String e1; 
+
+    public String r2; 
+
+    public String r3; 
+
+    public String i4; 
+
+    public String t5; 
+
+    public String o6; 
+
+    public String r7; 
+
+    public String i8; 
+
+    public String a9; 
+
+    public String l10; 
+
+}
