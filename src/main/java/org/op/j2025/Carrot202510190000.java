2b39eeabfe8c236ecb21c68dcc00d04a6d84ed5e
xx008888 <xx008888@outlook.com>
2025-10-19 08:00:57 +0800

    add Carrot202510190000

---
+++ src/main/java/org/op/j2025/Carrot202510190000.java
@@ -0,0 +1,17 @@
+package org.op.j2025;
+
+public class Carrot202510190000 {
+
+    public String c0; 
+
+    public String a1; 
+
+    public String r2; 
+
+    public String r3; 
+
+    public String o4; 
+
+    public String t5; 
+
+}
