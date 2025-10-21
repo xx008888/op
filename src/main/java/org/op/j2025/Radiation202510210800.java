8c3a33551f6af144dc82ee10a35931d0799740f3
xx008888 <xx008888@outlook.com>
2025-10-21 16:00:06 +0800

    add Radiation202510210800

---
+++ src/main/java/org/op/j2025/Radiation202510210800.java
@@ -0,0 +1,23 @@
+package org.op.j2025;
+
+public class Radiation202510210800 {
+
+    public String r0; 
+
+    public String a1; 
+
+    public String d2; 
+
+    public String i3; 
+
+    public String a4; 
+
+    public String t5; 
+
+    public String i6; 
+
+    public String o7; 
+
+    public String n8; 
+
+}
