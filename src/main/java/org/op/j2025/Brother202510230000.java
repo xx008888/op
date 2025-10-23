2f3fa06609421cf60757ca6ffe827d9603197da9
xx008888 <xx008888@outlook.com>
2025-10-23 08:00:33 +0800

    add Brother202510230000

---
+++ src/main/java/org/op/j2025/Brother202510230000.java
@@ -0,0 +1,19 @@
+package org.op.j2025;
+
+public class Brother202510230000 {
+
+    public String b0; 
+
+    public String r1; 
+
+    public String o2; 
+
+    public String t3; 
+
+    public String h4; 
+
+    public String e5; 
+
+    public String r6; 
+
+}
