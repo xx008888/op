8c56006219afb4128ff10ece4e52f41b56067a69
xx008888 <xx008888@outlook.com>
2025-10-25 08:00:53 +0800

    add Normalise202510250000

---
+++ src/main/java/org/op/j2025/Normalise202510250000.java
@@ -0,0 +1,23 @@
+package org.op.j2025;
+
+public class Normalise202510250000 {
+
+    public String n0; 
+
+    public String o1; 
+
+    public String r2; 
+
+    public String m3; 
+
+    public String a4; 
+
+    public String l5; 
+
+    public String i6; 
+
+    public String s7; 
+
+    public String e8; 
+
+}
