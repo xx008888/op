d780bf4d990e5e9240c6493f78d6a819d862199e
xx008888 <xx008888@outlook.com>
2025-10-30 00:00:12 +0800

    add School202510291600

---
+++ src/main/java/org/op/j2025/School202510291600.java
@@ -0,0 +1,17 @@
+package org.op.j2025;
+
+public class School202510291600 {
+
+    public String s0; 
+
+    public String c1; 
+
+    public String h2; 
+
+    public String o3; 
+
+    public String o4; 
+
+    public String l5; 
+
+}
