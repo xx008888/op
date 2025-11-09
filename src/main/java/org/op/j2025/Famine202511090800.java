fd6e3d538da8492fb3e1c5ffcbdfba310686c239
xx008888 <xx008888@outlook.com>
2025-11-09 16:00:09 +0800

    add Famine202511090800

---
+++ src/main/java/org/op/j2025/Famine202511090800.java
@@ -0,0 +1,17 @@
+package org.op.j2025;
+
+public class Famine202511090800 {
+
+    public String f0; 
+
+    public String a1; 
+
+    public String m2; 
+
+    public String i3; 
+
+    public String n4; 
+
+    public String e5; 
+
+}
