bc6fd935545c25b0841c01ad212ce2148de04772
xx008888 <xx008888@outlook.com>
2025-11-10 08:00:40 +0800

    add Ivory202511100000

---
+++ src/main/java/org/op/j2025/Ivory202511100000.java
@@ -0,0 +1,15 @@
+package org.op.j2025;
+
+public class Ivory202511100000 {
+
+    public String i0; 
+
+    public String v1; 
+
+    public String o2; 
+
+    public String r3; 
+
+    public String y4; 
+
+}
