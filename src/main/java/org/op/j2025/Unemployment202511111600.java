6f0d188672553e7000a89a776498345540e8580d
xx008888 <xx008888@outlook.com>
2025-11-12 00:00:18 +0800

    add Unemployment202511111600

---
+++ src/main/java/org/op/j2025/Unemployment202511111600.java
@@ -0,0 +1,29 @@
+package org.op.j2025;
+
+public class Unemployment202511111600 {
+
+    public String u0; 
+
+    public String n1; 
+
+    public String e2; 
+
+    public String m3; 
+
+    public String p4; 
+
+    public String l5; 
+
+    public String o6; 
+
+    public String y7; 
+
+    public String m8; 
+
+    public String e9; 
+
+    public String n10; 
+
+    public String t11; 
+
+}
