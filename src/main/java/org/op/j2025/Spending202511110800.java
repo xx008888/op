93bcb8b4c61aab3c673edc40bbd5d94fa6a2440f
xx008888 <xx008888@outlook.com>
2025-11-11 16:00:32 +0800

    add Spending202511110800

---
+++ src/main/java/org/op/j2025/Spending202511110800.java
@@ -0,0 +1,21 @@
+package org.op.j2025;
+
+public class Spending202511110800 {
+
+    public String s0; 
+
+    public String p1; 
+
+    public String e2; 
+
+    public String n3; 
+
+    public String d4; 
+
+    public String i5; 
+
+    public String n6; 
+
+    public String g7; 
+
+}
