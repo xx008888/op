bf7afa79d2e7be4e6e116cb8ee44ea1b99e36bae
xx008888 <xx008888@outlook.com>
2025-11-13 00:00:24 +0800

    add Conqueror202511121600

---
+++ src/main/java/org/op/j2025/Conqueror202511121600.java
@@ -0,0 +1,23 @@
+package org.op.j2025;
+
+public class Conqueror202511121600 {
+
+    public String c0; 
+
+    public String o1; 
+
+    public String n2; 
+
+    public String q3; 
+
+    public String u4; 
+
+    public String e5; 
+
+    public String r6; 
+
+    public String o7; 
+
+    public String r8; 
+
+}
