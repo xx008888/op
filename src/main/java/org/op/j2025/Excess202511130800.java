8e65ddfa0bd381ba6843d7a53726268ed9bbdbc7
xx008888 <xx008888@outlook.com>
2025-11-13 16:00:32 +0800

    add Excess202511130800

---
+++ src/main/java/org/op/j2025/Excess202511130800.java
@@ -0,0 +1,17 @@
+package org.op.j2025;
+
+public class Excess202511130800 {
+
+    public String e0; 
+
+    public String x1; 
+
+    public String c2; 
+
+    public String e3; 
+
+    public String s4; 
+
+    public String s5; 
+
+}
