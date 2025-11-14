ef4e985ab089d90eda9c8c9e77c54dc14ed5c742
xx008888 <xx008888@outlook.com>
2025-11-14 16:00:40 +0800

    add Money202511140800

---
+++ src/main/java/org/op/j2025/Money202511140800.java
@@ -0,0 +1,15 @@
+package org.op.j2025;
+
+public class Money202511140800 {
+
+    public String m0; 
+
+    public String o1; 
+
+    public String n2; 
+
+    public String e3; 
+
+    public String y4; 
+
+}
