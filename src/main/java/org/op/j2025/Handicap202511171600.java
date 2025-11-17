52bc5f83a48a981ebf94160a76ee5ede7d6f2538
xx008888 <xx008888@outlook.com>
2025-11-18 00:00:14 +0800

    add Handicap202511171600

---
+++ src/main/java/org/op/j2025/Handicap202511171600.java
@@ -0,0 +1,21 @@
+package org.op.j2025;
+
+public class Handicap202511171600 {
+
+    public String h0; 
+
+    public String a1; 
+
+    public String n2; 
+
+    public String d3; 
+
+    public String i4; 
+
+    public String c5; 
+
+    public String a6; 
+
+    public String p7; 
+
+}
