b602331666d471666d38555af282e0def2bc7b13
xx008888 <xx008888@outlook.com>
2025-11-17 16:00:22 +0800

    add Enterprise202511170800

---
+++ src/main/java/org/op/j2025/Enterprise202511170800.java
@@ -0,0 +1,25 @@
+package org.op.j2025;
+
+public class Enterprise202511170800 {
+
+    public String e0; 
+
+    public String n1; 
+
+    public String t2; 
+
+    public String e3; 
+
+    public String r4; 
+
+    public String p5; 
+
+    public String r6; 
+
+    public String i7; 
+
+    public String s8; 
+
+    public String e9; 
+
+}
