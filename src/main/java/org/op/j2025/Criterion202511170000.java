11d73b5865077d67a426203dc1a80d5d08932b8e
xx008888 <xx008888@outlook.com>
2025-11-17 08:00:21 +0800

    add Criterion202511170000

---
+++ src/main/java/org/op/j2025/Criterion202511170000.java
@@ -0,0 +1,23 @@
+package org.op.j2025;
+
+public class Criterion202511170000 {
+
+    public String c0; 
+
+    public String r1; 
+
+    public String i2; 
+
+    public String t3; 
+
+    public String e4; 
+
+    public String r5; 
+
+    public String i6; 
+
+    public String o7; 
+
+    public String n8; 
+
+}
