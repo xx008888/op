fe5d4e11698b2d372759e8e26dd40ed5bed91b29
xx008888 <xx008888@outlook.com>
2025-11-18 08:00:53 +0800

    add Income202511180000

---
+++ src/main/java/org/op/j2025/Income202511180000.java
@@ -0,0 +1,17 @@
+package org.op.j2025;
+
+public class Income202511180000 {
+
+    public String i0; 
+
+    public String n1; 
+
+    public String c2; 
+
+    public String o3; 
+
+    public String m4; 
+
+    public String e5; 
+
+}
