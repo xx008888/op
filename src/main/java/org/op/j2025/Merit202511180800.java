dc12ca7b980bc19a8be3c0f3b7e6ffd8ddc00f0d
xx008888 <xx008888@outlook.com>
2025-11-18 16:00:46 +0800

    add Merit202511180800

---
+++ src/main/java/org/op/j2025/Merit202511180800.java
@@ -0,0 +1,15 @@
+package org.op.j2025;
+
+public class Merit202511180800 {
+
+    public String m0; 
+
+    public String e1; 
+
+    public String r2; 
+
+    public String i3; 
+
+    public String t4; 
+
+}
