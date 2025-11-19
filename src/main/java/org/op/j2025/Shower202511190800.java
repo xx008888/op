05deede4f6bcde4832aed54d1da5e8720a3dd351
xx008888 <xx008888@outlook.com>
2025-11-19 16:00:53 +0800

    add Shower202511190800

---
+++ src/main/java/org/op/j2025/Shower202511190800.java
@@ -0,0 +1,17 @@
+package org.op.j2025;
+
+public class Shower202511190800 {
+
+    public String s0; 
+
+    public String h1; 
+
+    public String o2; 
+
+    public String w3; 
+
+    public String e4; 
+
+    public String r5; 
+
+}
