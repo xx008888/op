fda2308d52ff1cf0b39f4dba8a1d5087c173005a
xx008888 <xx008888@outlook.com>
2025-11-20 16:00:57 +0800

    add Artery202511200800

---
+++ src/main/java/org/op/j2025/Artery202511200800.java
@@ -0,0 +1,17 @@
+package org.op.j2025;
+
+public class Artery202511200800 {
+
+    public String a0; 
+
+    public String r1; 
+
+    public String t2; 
+
+    public String e3; 
+
+    public String r4; 
+
+    public String y5; 
+
+}
