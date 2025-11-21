bfb87449b73e6da38f353ee707bf977d6f0aeb7e
xx008888 <xx008888@outlook.com>
2025-11-21 08:00:08 +0800

    add Convert202511210000

---
+++ src/main/java/org/op/j2025/Convert202511210000.java
@@ -0,0 +1,19 @@
+package org.op.j2025;
+
+public class Convert202511210000 {
+
+    public String c0; 
+
+    public String o1; 
+
+    public String n2; 
+
+    public String v3; 
+
+    public String e4; 
+
+    public String r5; 
+
+    public String t6; 
+
+}
