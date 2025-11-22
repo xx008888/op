fbaa03e41ea38db836137d60df41e64635b9f038
xx008888 <xx008888@outlook.com>
2025-11-23 00:00:12 +0800

    add Particle202511221600

---
+++ src/main/java/org/op/j2025/Particle202511221600.java
@@ -0,0 +1,21 @@
+package org.op.j2025;
+
+public class Particle202511221600 {
+
+    public String p0; 
+
+    public String a1; 
+
+    public String r2; 
+
+    public String t3; 
+
+    public String i4; 
+
+    public String c5; 
+
+    public String l6; 
+
+    public String e7; 
+
+}
