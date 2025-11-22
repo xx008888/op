4dddd729f494585023ef1497ea57597fbf958321
xx008888 <xx008888@outlook.com>
2025-11-22 08:00:11 +0800

    add Hurry202511220000

---
+++ src/main/java/org/op/j2025/Hurry202511220000.java
@@ -0,0 +1,15 @@
+package org.op.j2025;
+
+public class Hurry202511220000 {
+
+    public String h0; 
+
+    public String u1; 
+
+    public String r2; 
+
+    public String r3; 
+
+    public String y4; 
+
+}
