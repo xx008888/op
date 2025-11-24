00e088a11c333f251c46a319bcbd8c4a3c95cdc2
xx008888 <xx008888@outlook.com>
2025-11-24 16:00:23 +0800

    add Angry202511240800

---
+++ src/main/java/org/op/j2025/Angry202511240800.java
@@ -0,0 +1,15 @@
+package org.op.j2025;
+
+public class Angry202511240800 {
+
+    public String a0; 
+
+    public String n1; 
+
+    public String g2; 
+
+    public String r3; 
+
+    public String y4; 
+
+}
