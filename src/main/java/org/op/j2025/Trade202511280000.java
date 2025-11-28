623e1ad03e52902e4a8c67bc72f13e7348f560f1
xx008888 <xx008888@outlook.com>
2025-11-28 08:00:32 +0800

    add Trade202511280000

---
+++ src/main/java/org/op/j2025/Trade202511280000.java
@@ -0,0 +1,15 @@
+package org.op.j2025;
+
+public class Trade202511280000 {
+
+    public String t0; 
+
+    public String r1; 
+
+    public String a2; 
+
+    public String d3; 
+
+    public String e4; 
+
+}
