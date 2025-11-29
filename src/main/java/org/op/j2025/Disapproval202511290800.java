450f7b8f43fb7d29ff553a14a59aac53726fa342
xx008888 <xx008888@outlook.com>
2025-11-29 16:00:30 +0800

    add Disapproval202511290800

---
+++ src/main/java/org/op/j2025/Disapproval202511290800.java
@@ -0,0 +1,27 @@
+package org.op.j2025;
+
+public class Disapproval202511290800 {
+
+    public String d0; 
+
+    public String i1; 
+
+    public String s2; 
+
+    public String a3; 
+
+    public String p4; 
+
+    public String p5; 
+
+    public String r6; 
+
+    public String o7; 
+
+    public String v8; 
+
+    public String a9; 
+
+    public String l10; 
+
+}
