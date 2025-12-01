cd168449666b65c0791174ac5c7314c2c79bd383
xx008888 <xx008888@outlook.com>
2025-12-02 00:00:40 +0800

    add Attempt202512011600

---
+++ src/main/java/org/op/j2025/Attempt202512011600.java
@@ -0,0 +1,19 @@
+package org.op.j2025;
+
+public class Attempt202512011600 {
+
+    public String a0; 
+
+    public String t1; 
+
+    public String t2; 
+
+    public String e3; 
+
+    public String m4; 
+
+    public String p5; 
+
+    public String t6; 
+
+}
