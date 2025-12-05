7bb24728bc098decbadf8be2cd13177009ba7b53
xx008888 <xx008888@outlook.com>
2025-12-05 08:00:25 +0800

    add Spring202512050000

---
+++ src/main/java/org/op/j2025/Spring202512050000.java
@@ -0,0 +1,17 @@
+package org.op.j2025;
+
+public class Spring202512050000 {
+
+    public String s0; 
+
+    public String p1; 
+
+    public String r2; 
+
+    public String i3; 
+
+    public String n4; 
+
+    public String g5; 
+
+}
