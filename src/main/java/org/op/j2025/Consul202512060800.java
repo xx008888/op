d1c3f56748e8762c8c234daa2c2f8bf182b437f8
xx008888 <xx008888@outlook.com>
2025-12-06 16:00:27 +0800

    add Consul202512060800

---
+++ src/main/java/org/op/j2025/Consul202512060800.java
@@ -0,0 +1,17 @@
+package org.op.j2025;
+
+public class Consul202512060800 {
+
+    public String c0; 
+
+    public String o1; 
+
+    public String n2; 
+
+    public String s3; 
+
+    public String u4; 
+
+    public String l5; 
+
+}
