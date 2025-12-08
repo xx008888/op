690396df63900ef428d205433e7b73c20d8ae3dd
xx008888 <xx008888@outlook.com>
2025-12-08 16:00:25 +0800

    add Poultry202512080800

---
+++ src/main/java/org/op/j2025/Poultry202512080800.java
@@ -0,0 +1,19 @@
+package org.op.j2025;
+
+public class Poultry202512080800 {
+
+    public String p0; 
+
+    public String o1; 
+
+    public String u2; 
+
+    public String l3; 
+
+    public String t4; 
+
+    public String r5; 
+
+    public String y6; 
+
+}
