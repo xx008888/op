493b152f9ab62151b837d5f0a976a53c517c2bdf
xx008888 <xx008888@outlook.com>
2025-12-13 08:00:39 +0800

    add Simply202512130000

---
+++ src/main/java/org/op/j2025/Simply202512130000.java
@@ -0,0 +1,17 @@
+package org.op.j2025;
+
+public class Simply202512130000 {
+
+    public String s0; 
+
+    public String i1; 
+
+    public String m2; 
+
+    public String p3; 
+
+    public String l4; 
+
+    public String y5; 
+
+}
