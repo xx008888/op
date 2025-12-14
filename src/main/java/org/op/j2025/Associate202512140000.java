5744b433e9c4b3219e89311ef5bbadbcc1f71afa
xx008888 <xx008888@outlook.com>
2025-12-14 08:00:46 +0800

    add Associate202512140000

---
+++ src/main/java/org/op/j2025/Associate202512140000.java
@@ -0,0 +1,23 @@
+package org.op.j2025;
+
+public class Associate202512140000 {
+
+    public String a0; 
+
+    public String s1; 
+
+    public String s2; 
+
+    public String o3; 
+
+    public String c4; 
+
+    public String i5; 
+
+    public String a6; 
+
+    public String t7; 
+
+    public String e8; 
+
+}
