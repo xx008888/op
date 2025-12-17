fdc61643f9d2c7aad1118df14994234aff97d60b
xx008888 <xx008888@outlook.com>
2025-12-17 16:00:28 +0800

    add Temper202512170800

---
+++ src/main/java/org/op/j2025/Temper202512170800.java
@@ -0,0 +1,17 @@
+package org.op.j2025;
+
+public class Temper202512170800 {
+
+    public String t0; 
+
+    public String e1; 
+
+    public String m2; 
+
+    public String p3; 
+
+    public String e4; 
+
+    public String r5; 
+
+}
