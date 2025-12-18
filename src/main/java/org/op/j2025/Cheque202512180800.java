8c911491ca0274be0ce681193a4d5472c7d018f9
xx008888 <xx008888@outlook.com>
2025-12-18 16:00:37 +0800

    add Cheque202512180800

---
+++ src/main/java/org/op/j2025/Cheque202512180800.java
@@ -0,0 +1,17 @@
+package org.op.j2025;
+
+public class Cheque202512180800 {
+
+    public String c0; 
+
+    public String h1; 
+
+    public String e2; 
+
+    public String q3; 
+
+    public String u4; 
+
+    public String e5; 
+
+}
