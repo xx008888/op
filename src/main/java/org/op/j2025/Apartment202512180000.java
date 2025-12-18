b6346586edf05ff189e4aa5f903df637ebfbaf61
xx008888 <xx008888@outlook.com>
2025-12-18 08:00:38 +0800

    add Apartment202512180000

---
+++ src/main/java/org/op/j2025/Apartment202512180000.java
@@ -0,0 +1,23 @@
+package org.op.j2025;
+
+public class Apartment202512180000 {
+
+    public String a0; 
+
+    public String p1; 
+
+    public String a2; 
+
+    public String r3; 
+
+    public String t4; 
+
+    public String m5; 
+
+    public String e6; 
+
+    public String n7; 
+
+    public String t8; 
+
+}
