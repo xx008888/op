cd4ae5b55f5e7b970226743c2fc2a37343b9ae62
xx008888 <xx008888@outlook.com>
2025-12-25 08:00:15 +0800

    add Revenue202512250000

---
+++ src/main/java/org/op/j2025/Revenue202512250000.java
@@ -0,0 +1,19 @@
+package org.op.j2025;
+
+public class Revenue202512250000 {
+
+    public String r0; 
+
+    public String e1; 
+
+    public String v2; 
+
+    public String e3; 
+
+    public String n4; 
+
+    public String u5; 
+
+    public String e6; 
+
+}
