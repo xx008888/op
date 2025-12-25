0286691af9893a494eec176db6ccd2235592a0f9
xx008888 <xx008888@outlook.com>
2025-12-25 16:00:14 +0800

    add Straw202512250800

---
+++ src/main/java/org/op/j2025/Straw202512250800.java
@@ -0,0 +1,15 @@
+package org.op.j2025;
+
+public class Straw202512250800 {
+
+    public String s0; 
+
+    public String t1; 
+
+    public String r2; 
+
+    public String a3; 
+
+    public String w4; 
+
+}
