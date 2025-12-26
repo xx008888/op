8538dd8f9256bf95c0c4702f36eeb8a4b950313c
xx008888 <xx008888@outlook.com>
2025-12-26 16:00:20 +0800

    add Breed202512260800

---
+++ src/main/java/org/op/j2025/Breed202512260800.java
@@ -0,0 +1,15 @@
+package org.op.j2025;
+
+public class Breed202512260800 {
+
+    public String b0; 
+
+    public String r1; 
+
+    public String e2; 
+
+    public String e3; 
+
+    public String d4; 
+
+}
