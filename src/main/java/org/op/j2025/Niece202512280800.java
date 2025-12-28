a070b175ded5ca14c96885bcae24036977fd821a
xx008888 <xx008888@outlook.com>
2025-12-28 16:00:26 +0800

    add Niece202512280800

---
+++ src/main/java/org/op/j2025/Niece202512280800.java
@@ -0,0 +1,15 @@
+package org.op.j2025;
+
+public class Niece202512280800 {
+
+    public String n0; 
+
+    public String i1; 
+
+    public String e2; 
+
+    public String c3; 
+
+    public String e4; 
+
+}
