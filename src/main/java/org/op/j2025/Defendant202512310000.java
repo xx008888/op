cff928f5f6ef2f4dccc5345ddd13fb1923f0b68c
xx008888 <xx008888@outlook.com>
2025-12-31 08:00:11 +0800

    add Defendant202512310000

---
+++ src/main/java/org/op/j2025/Defendant202512310000.java
@@ -0,0 +1,23 @@
+package org.op.j2025;
+
+public class Defendant202512310000 {
+
+    public String d0; 
+
+    public String e1; 
+
+    public String f2; 
+
+    public String e3; 
+
+    public String n4; 
+
+    public String d5; 
+
+    public String a6; 
+
+    public String n7; 
+
+    public String t8; 
+
+}
