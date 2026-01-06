ba41b359101d323126ef01c560339c94bdae1abd
xx008888 <xx008888@outlook.com>
2026-01-06 08:00:30 +0800

    add Plead202601060000

---
+++ src/main/java/org/op/j2026/Plead202601060000.java
@@ -0,0 +1,15 @@
+package org.op.j2026;
+
+public class Plead202601060000 {
+
+    public String p0; 
+
+    public String l1; 
+
+    public String e2; 
+
+    public String a3; 
+
+    public String d4; 
+
+}
