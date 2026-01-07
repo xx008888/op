7b755d59673b3f45a75485e0df3dc8b5d7f7b975
xx008888 <xx008888@outlook.com>
2026-01-07 16:00:20 +0800

    add Affordability202601070800

---
+++ src/main/java/org/op/j2026/Affordability202601070800.java
@@ -0,0 +1,31 @@
+package org.op.j2026;
+
+public class Affordability202601070800 {
+
+    public String a0; 
+
+    public String f1; 
+
+    public String f2; 
+
+    public String o3; 
+
+    public String r4; 
+
+    public String d5; 
+
+    public String a6; 
+
+    public String b7; 
+
+    public String i8; 
+
+    public String l9; 
+
+    public String i10; 
+
+    public String t11; 
+
+    public String y12; 
+
+}
