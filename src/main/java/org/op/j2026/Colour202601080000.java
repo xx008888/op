0013ea8cf52c8c87d92777ff4760ac13554abca4
xx008888 <xx008888@outlook.com>
2026-01-08 08:00:15 +0800

    add Colour202601080000

---
+++ src/main/java/org/op/j2026/Colour202601080000.java
@@ -0,0 +1,17 @@
+package org.op.j2026;
+
+public class Colour202601080000 {
+
+    public String c0; 
+
+    public String o1; 
+
+    public String l2; 
+
+    public String o3; 
+
+    public String u4; 
+
+    public String r5; 
+
+}
