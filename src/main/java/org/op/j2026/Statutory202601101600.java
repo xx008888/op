25ffa91bd48780709c6a2f9bdcd02d5baa2254d8
xx008888 <xx008888@outlook.com>
2026-01-11 00:00:25 +0800

    add Statutory202601101600

---
+++ src/main/java/org/op/j2026/Statutory202601101600.java
@@ -0,0 +1,23 @@
+package org.op.j2026;
+
+public class Statutory202601101600 {
+
+    public String s0; 
+
+    public String t1; 
+
+    public String a2; 
+
+    public String t3; 
+
+    public String u4; 
+
+    public String t5; 
+
+    public String o6; 
+
+    public String r7; 
+
+    public String y8; 
+
+}
