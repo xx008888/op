88cb7da0751da6e2130a97c78111f9a7a49a73f2
xx008888 <xx008888@outlook.com>
2026-01-11 16:00:24 +0800

    add Accident202601110800

---
+++ src/main/java/org/op/j2026/Accident202601110800.java
@@ -0,0 +1,21 @@
+package org.op.j2026;
+
+public class Accident202601110800 {
+
+    public String a0; 
+
+    public String c1; 
+
+    public String c2; 
+
+    public String i3; 
+
+    public String d4; 
+
+    public String e5; 
+
+    public String n6; 
+
+    public String t7; 
+
+}
