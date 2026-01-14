c6dc328a5e495c2654bf8794a81ac5f389752081
xx008888 <xx008888@outlook.com>
2026-01-14 16:00:27 +0800

    add Refusal202601140800

---
+++ src/main/java/org/op/j2026/Refusal202601140800.java
@@ -0,0 +1,19 @@
+package org.op.j2026;
+
+public class Refusal202601140800 {
+
+    public String r0; 
+
+    public String e1; 
+
+    public String f2; 
+
+    public String u3; 
+
+    public String s4; 
+
+    public String a5; 
+
+    public String l6; 
+
+}
