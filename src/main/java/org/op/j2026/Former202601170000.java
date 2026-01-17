77c02968498357799de1ebb8f78aa015a28ea383
xx008888 <xx008888@outlook.com>
2026-01-17 08:00:46 +0800

    add Former202601170000

---
+++ src/main/java/org/op/j2026/Former202601170000.java
@@ -0,0 +1,17 @@
+package org.op.j2026;
+
+public class Former202601170000 {
+
+    public String f0; 
+
+    public String o1; 
+
+    public String r2; 
+
+    public String m3; 
+
+    public String e4; 
+
+    public String r5; 
+
+}
