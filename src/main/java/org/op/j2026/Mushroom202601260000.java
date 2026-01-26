c893743eb4d7364d3298cde6e735745827d996b2
xx008888 <xx008888@outlook.com>
2026-01-26 08:00:44 +0800

    add Mushroom202601260000

---
+++ src/main/java/org/op/j2026/Mushroom202601260000.java
@@ -0,0 +1,21 @@
+package org.op.j2026;
+
+public class Mushroom202601260000 {
+
+    public String m0; 
+
+    public String u1; 
+
+    public String s2; 
+
+    public String h3; 
+
+    public String r4; 
+
+    public String o5; 
+
+    public String o6; 
+
+    public String m7; 
+
+}
