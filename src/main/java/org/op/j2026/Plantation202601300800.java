0e8ca1c15272728ff10e41a4cdc08c77d97df58a
xx008888 <xx008888@outlook.com>
2026-01-30 16:00:55 +0800

    add Plantation202601300800

---
+++ src/main/java/org/op/j2026/Plantation202601300800.java
@@ -0,0 +1,25 @@
+package org.op.j2026;
+
+public class Plantation202601300800 {
+
+    public String p0; 
+
+    public String l1; 
+
+    public String a2; 
+
+    public String n3; 
+
+    public String t4; 
+
+    public String a5; 
+
+    public String t6; 
+
+    public String i7; 
+
+    public String o8; 
+
+    public String n9; 
+
+}
