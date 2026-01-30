b067f7558d7795acaf47bc7944e8f115adc122b4
xx008888 <xx008888@outlook.com>
2026-01-30 08:00:57 +0800

    add Minus202601300000

---
+++ src/main/java/org/op/j2026/Minus202601300000.java
@@ -0,0 +1,15 @@
+package org.op.j2026;
+
+public class Minus202601300000 {
+
+    public String m0; 
+
+    public String i1; 
+
+    public String n2; 
+
+    public String u3; 
+
+    public String s4; 
+
+}
