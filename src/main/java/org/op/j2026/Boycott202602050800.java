8dbfc5246e57722388ce7eff5bb6c49e9ed76772
xx008888 <xx008888@outlook.com>
2026-02-05 16:00:51 +0800

    add Boycott202602050800

---
+++ src/main/java/org/op/j2026/Boycott202602050800.java
@@ -0,0 +1,19 @@
+package org.op.j2026;
+
+public class Boycott202602050800 {
+
+    public String b0; 
+
+    public String o1; 
+
+    public String y2; 
+
+    public String c3; 
+
+    public String o4; 
+
+    public String t5; 
+
+    public String t6; 
+
+}
