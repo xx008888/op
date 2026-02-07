51d2debb4eb0b960b5525248c47921be067b63c7
xx008888 <xx008888@outlook.com>
2026-02-07 16:00:05 +0800

    add Neither202602070800

---
+++ src/main/java/org/op/j2026/Neither202602070800.java
@@ -0,0 +1,19 @@
+package org.op.j2026;
+
+public class Neither202602070800 {
+
+    public String n0; 
+
+    public String e1; 
+
+    public String i2; 
+
+    public String t3; 
+
+    public String h4; 
+
+    public String e5; 
+
+    public String r6; 
+
+}
