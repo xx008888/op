f7721aaefa14acccdb13741cf3b801eb832079f4
xx008888 <xx008888@outlook.com>
2026-02-08 08:00:27 +0800

    add Relationship202602080000

---
+++ src/main/java/org/op/j2026/Relationship202602080000.java
@@ -0,0 +1,29 @@
+package org.op.j2026;
+
+public class Relationship202602080000 {
+
+    public String r0; 
+
+    public String e1; 
+
+    public String l2; 
+
+    public String a3; 
+
+    public String t4; 
+
+    public String i5; 
+
+    public String o6; 
+
+    public String n7; 
+
+    public String s8; 
+
+    public String h9; 
+
+    public String i10; 
+
+    public String p11; 
+
+}
