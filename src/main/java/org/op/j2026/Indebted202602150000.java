34aada4ec21d8a50396d66242f7b2aed158f2647
xx008888 <xx008888@outlook.com>
2026-02-15 08:00:18 +0800

    add Indebted202602150000

---
+++ src/main/java/org/op/j2026/Indebted202602150000.java
@@ -0,0 +1,21 @@
+package org.op.j2026;
+
+public class Indebted202602150000 {
+
+    public String i0; 
+
+    public String n1; 
+
+    public String d2; 
+
+    public String e3; 
+
+    public String b4; 
+
+    public String t5; 
+
+    public String e6; 
+
+    public String d7; 
+
+}
