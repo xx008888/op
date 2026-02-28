d029360663f03e5d1bd3791d0e18b97f2409f930
xx008888 <xx008888@outlook.com>
2026-02-28 16:00:21 +0800

    add Restrictive202602280800

---
+++ src/main/java/org/op/j2026/Restrictive202602280800.java
@@ -0,0 +1,27 @@
+package org.op.j2026;
+
+public class Restrictive202602280800 {
+
+    public String r0; 
+
+    public String e1; 
+
+    public String s2; 
+
+    public String t3; 
+
+    public String r4; 
+
+    public String i5; 
+
+    public String c6; 
+
+    public String t7; 
+
+    public String i8; 
+
+    public String v9; 
+
+    public String e10; 
+
+}
