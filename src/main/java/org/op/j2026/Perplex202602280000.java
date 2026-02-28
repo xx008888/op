a59b2778f4e90dad5e6fee0370ef4328a27f1872
xx008888 <xx008888@outlook.com>
2026-02-28 08:00:59 +0800

    add Perplex202602280000

---
+++ src/main/java/org/op/j2026/Perplex202602280000.java
@@ -0,0 +1,19 @@
+package org.op.j2026;
+
+public class Perplex202602280000 {
+
+    public String p0; 
+
+    public String e1; 
+
+    public String r2; 
+
+    public String p3; 
+
+    public String l4; 
+
+    public String e5; 
+
+    public String x6; 
+
+}
