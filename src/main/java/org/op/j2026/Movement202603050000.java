a9e5b2044c2be03c1a35581d038f7f523495162d
xx008888 <xx008888@outlook.com>
2026-03-05 08:00:57 +0800

    add Movement202603050000

---
+++ src/main/java/org/op/j2026/Movement202603050000.java
@@ -0,0 +1,21 @@
+package org.op.j2026;
+
+public class Movement202603050000 {
+
+    public String m0; 
+
+    public String o1; 
+
+    public String v2; 
+
+    public String e3; 
+
+    public String m4; 
+
+    public String e5; 
+
+    public String n6; 
+
+    public String t7; 
+
+}
