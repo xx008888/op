53354c692a3c514d5603cbf0bec146665815d7f7
xx008888 <xx008888@outlook.com>
2026-03-05 16:00:42 +0800

    add Practical202603050800

---
+++ src/main/java/org/op/j2026/Practical202603050800.java
@@ -0,0 +1,23 @@
+package org.op.j2026;
+
+public class Practical202603050800 {
+
+    public String p0; 
+
+    public String r1; 
+
+    public String a2; 
+
+    public String c3; 
+
+    public String t4; 
+
+    public String i5; 
+
+    public String c6; 
+
+    public String a7; 
+
+    public String l8; 
+
+}
