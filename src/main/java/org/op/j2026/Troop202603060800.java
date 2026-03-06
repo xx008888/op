75a1b87ca3772c1e074ab11acec85f42fb40c0fe
xx008888 <xx008888@outlook.com>
2026-03-06 16:00:58 +0800

    add Troop202603060800

---
+++ src/main/java/org/op/j2026/Troop202603060800.java
@@ -0,0 +1,15 @@
+package org.op.j2026;
+
+public class Troop202603060800 {
+
+    public String t0; 
+
+    public String r1; 
+
+    public String o2; 
+
+    public String o3; 
+
+    public String p4; 
+
+}
