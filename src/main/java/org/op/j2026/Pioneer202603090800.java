e944ca2cb7a2985679eb59f40b97acf3e8726808
xx008888 <xx008888@outlook.com>
2026-03-09 16:00:35 +0800

    add Pioneer202603090800

---
+++ src/main/java/org/op/j2026/Pioneer202603090800.java
@@ -0,0 +1,19 @@
+package org.op.j2026;
+
+public class Pioneer202603090800 {
+
+    public String p0; 
+
+    public String i1; 
+
+    public String o2; 
+
+    public String n3; 
+
+    public String e4; 
+
+    public String e5; 
+
+    public String r6; 
+
+}
