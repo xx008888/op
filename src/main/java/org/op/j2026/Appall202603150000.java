c651cc57685ce9a1055660e80f91c7afcb27f1be
xx008888 <xx008888@outlook.com>
2026-03-15 08:00:48 +0800

    add Appall202603150000

---
+++ src/main/java/org/op/j2026/Appall202603150000.java
@@ -0,0 +1,17 @@
+package org.op.j2026;
+
+public class Appall202603150000 {
+
+    public String a0; 
+
+    public String p1; 
+
+    public String p2; 
+
+    public String a3; 
+
+    public String l4; 
+
+    public String l5; 
+
+}
