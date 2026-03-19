e480fd2cded2866a6ff144191bd312bddb0e53d1
xx008888 <xx008888@outlook.com>
2026-03-19 16:00:10 +0800

    add Capitalism202603190800

---
+++ src/main/java/org/op/j2026/Capitalism202603190800.java
@@ -0,0 +1,25 @@
+package org.op.j2026;
+
+public class Capitalism202603190800 {
+
+    public String c0; 
+
+    public String a1; 
+
+    public String p2; 
+
+    public String i3; 
+
+    public String t4; 
+
+    public String a5; 
+
+    public String l6; 
+
+    public String i7; 
+
+    public String s8; 
+
+    public String m9; 
+
+}
