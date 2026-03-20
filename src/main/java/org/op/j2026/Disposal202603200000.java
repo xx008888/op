858664ce7e28aa3a24ee02c71556dc1818ab97bc
xx008888 <xx008888@outlook.com>
2026-03-20 08:00:43 +0800

    add Disposal202603200000

---
+++ src/main/java/org/op/j2026/Disposal202603200000.java
@@ -0,0 +1,21 @@
+package org.op.j2026;
+
+public class Disposal202603200000 {
+
+    public String d0; 
+
+    public String i1; 
+
+    public String s2; 
+
+    public String p3; 
+
+    public String o4; 
+
+    public String s5; 
+
+    public String a6; 
+
+    public String l7; 
+
+}
