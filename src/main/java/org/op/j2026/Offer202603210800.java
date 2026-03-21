754a39f6ec625c20d6eb6505326f2b1a32c0c761
xx008888 <xx008888@outlook.com>
2026-03-21 16:00:10 +0800

    add Offer202603210800

---
+++ src/main/java/org/op/j2026/Offer202603210800.java
@@ -0,0 +1,15 @@
+package org.op.j2026;
+
+public class Offer202603210800 {
+
+    public String o0; 
+
+    public String f1; 
+
+    public String f2; 
+
+    public String e3; 
+
+    public String r4; 
+
+}
