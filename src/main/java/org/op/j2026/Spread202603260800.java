b78c22a2861bec525a1ad054ea1f91cef16cb02e
xx008888 <xx008888@outlook.com>
2026-03-26 16:00:28 +0800

    add Spread202603260800

---
+++ src/main/java/org/op/j2026/Spread202603260800.java
@@ -0,0 +1,17 @@
+package org.op.j2026;
+
+public class Spread202603260800 {
+
+    public String s0; 
+
+    public String p1; 
+
+    public String r2; 
+
+    public String e3; 
+
+    public String a4; 
+
+    public String d5; 
+
+}
