cd987fcc83f9ace5450f22fe5edeb77357d882b1
xx008888 <xx008888@outlook.com>
2026-03-28 00:00:30 +0800

    add Consulate202603271600

---
+++ src/main/java/org/op/j2026/Consulate202603271600.java
@@ -0,0 +1,23 @@
+package org.op.j2026;
+
+public class Consulate202603271600 {
+
+    public String c0; 
+
+    public String o1; 
+
+    public String n2; 
+
+    public String s3; 
+
+    public String u4; 
+
+    public String l5; 
+
+    public String a6; 
+
+    public String t7; 
+
+    public String e8; 
+
+}
