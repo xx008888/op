8a8740b886d472f5aa76aa5b709074f58afdc237
xx008888 <xx008888@outlook.com>
2026-03-30 00:00:34 +0800

    add Pottery202603291600

---
+++ src/main/java/org/op/j2026/Pottery202603291600.java
@@ -0,0 +1,19 @@
+package org.op.j2026;
+
+public class Pottery202603291600 {
+
+    public String p0; 
+
+    public String o1; 
+
+    public String t2; 
+
+    public String t3; 
+
+    public String e4; 
+
+    public String r5; 
+
+    public String y6; 
+
+}
