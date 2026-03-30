814536086c8c49a4c75b2a3c243e6892f26d01a8
xx008888 <xx008888@outlook.com>
2026-03-30 08:00:17 +0800

    add Receipt202603300000

---
+++ src/main/java/org/op/j2026/Receipt202603300000.java
@@ -0,0 +1,19 @@
+package org.op.j2026;
+
+public class Receipt202603300000 {
+
+    public String r0; 
+
+    public String e1; 
+
+    public String c2; 
+
+    public String e3; 
+
+    public String i4; 
+
+    public String p5; 
+
+    public String t6; 
+
+}
