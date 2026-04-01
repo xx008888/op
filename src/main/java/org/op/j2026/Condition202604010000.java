132fa5476d9500351a139671095025257d5d59c6
xx008888 <xx008888@outlook.com>
2026-04-01 08:00:37 +0800

    add Condition202604010000

---
+++ src/main/java/org/op/j2026/Condition202604010000.java
@@ -0,0 +1,23 @@
+package org.op.j2026;
+
+public class Condition202604010000 {
+
+    public String c0; 
+
+    public String o1; 
+
+    public String n2; 
+
+    public String d3; 
+
+    public String i4; 
+
+    public String t5; 
+
+    public String i6; 
+
+    public String o7; 
+
+    public String n8; 
+
+}
