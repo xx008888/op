12f6f1e093535880b297d0b9739a6132f0404ea4
xx008888 <xx008888@outlook.com>
2026-04-03 16:00:42 +0800

    add Round202604030800

---
+++ src/main/java/org/op/j2026/Round202604030800.java
@@ -0,0 +1,15 @@
+package org.op.j2026;
+
+public class Round202604030800 {
+
+    public String r0; 
+
+    public String o1; 
+
+    public String u2; 
+
+    public String n3; 
+
+    public String d4; 
+
+}
