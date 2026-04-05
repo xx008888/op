7b92cf761c227dc3b5c8138e12c023207e4bbde0
xx008888 <xx008888@outlook.com>
2026-04-05 08:00:22 +0800

    add Combat202604050000

---
+++ src/main/java/org/op/j2026/Combat202604050000.java
@@ -0,0 +1,17 @@
+package org.op.j2026;
+
+public class Combat202604050000 {
+
+    public String c0; 
+
+    public String o1; 
+
+    public String m2; 
+
+    public String b3; 
+
+    public String a4; 
+
+    public String t5; 
+
+}
