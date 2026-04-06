35698dbe500cb72c143af3a4181511bb415ae873
xx008888 <xx008888@outlook.com>
2026-04-06 08:00:40 +0800

    add Gross202604060000

---
+++ src/main/java/org/op/j2026/Gross202604060000.java
@@ -0,0 +1,15 @@
+package org.op.j2026;
+
+public class Gross202604060000 {
+
+    public String g0; 
+
+    public String r1; 
+
+    public String o2; 
+
+    public String s3; 
+
+    public String s4; 
+
+}
