8df3223afc683331092f6c7f6eaf9722b43afd4c
xx008888 <xx008888@outlook.com>
2026-04-11 16:00:36 +0800

    add Regional202604110800

---
+++ src/main/java/org/op/j2026/Regional202604110800.java
@@ -0,0 +1,21 @@
+package org.op.j2026;
+
+public class Regional202604110800 {
+
+    public String r0; 
+
+    public String e1; 
+
+    public String g2; 
+
+    public String i3; 
+
+    public String o4; 
+
+    public String n5; 
+
+    public String a6; 
+
+    public String l7; 
+
+}
