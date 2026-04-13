03f2881e50655ec6624ee1606087d533aa6f1c58
xx008888 <xx008888@outlook.com>
2026-04-13 08:00:35 +0800

    add Catastrophe202604130000

---
+++ src/main/java/org/op/j2026/Catastrophe202604130000.java
@@ -0,0 +1,27 @@
+package org.op.j2026;
+
+public class Catastrophe202604130000 {
+
+    public String c0; 
+
+    public String a1; 
+
+    public String t2; 
+
+    public String a3; 
+
+    public String s4; 
+
+    public String t5; 
+
+    public String r6; 
+
+    public String o7; 
+
+    public String p8; 
+
+    public String h9; 
+
+    public String e10; 
+
+}
