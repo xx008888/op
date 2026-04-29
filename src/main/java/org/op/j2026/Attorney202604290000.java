53a7eea5d9215342eda059ec843361e595567d99
xx008888 <xx008888@outlook.com>
2026-04-29 08:00:56 +0800

    add Attorney202604290000

---
+++ src/main/java/org/op/j2026/Attorney202604290000.java
@@ -0,0 +1,21 @@
+package org.op.j2026;
+
+public class Attorney202604290000 {
+
+    public String a0; 
+
+    public String t1; 
+
+    public String t2; 
+
+    public String o3; 
+
+    public String r4; 
+
+    public String n5; 
+
+    public String e6; 
+
+    public String y7; 
+
+}
