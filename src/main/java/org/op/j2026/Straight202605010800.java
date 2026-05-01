1a1f9fef417fd3888e3546d0800e6f94fd45ba84
xx008888 <xx008888@outlook.com>
2026-05-01 16:00:23 +0800

    add Straight202605010800

---
+++ src/main/java/org/op/j2026/Straight202605010800.java
@@ -0,0 +1,21 @@
+package org.op.j2026;
+
+public class Straight202605010800 {
+
+    public String s0; 
+
+    public String t1; 
+
+    public String r2; 
+
+    public String a3; 
+
+    public String i4; 
+
+    public String g5; 
+
+    public String h6; 
+
+    public String t7; 
+
+}
