ffb976d43d84a01259af521dbad0e890ba210485
xx008888 <xx008888@outlook.com>
2026-05-07 16:00:36 +0800

    add Exemption202605070800

---
+++ src/main/java/org/op/j2026/Exemption202605070800.java
@@ -0,0 +1,23 @@
+package org.op.j2026;
+
+public class Exemption202605070800 {
+
+    public String e0; 
+
+    public String x1; 
+
+    public String e2; 
+
+    public String m3; 
+
+    public String p4; 
+
+    public String t5; 
+
+    public String i6; 
+
+    public String o7; 
+
+    public String n8; 
+
+}
