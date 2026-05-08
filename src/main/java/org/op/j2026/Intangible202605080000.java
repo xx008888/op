f5bc716cf79b2d11a48cf57b2ffb83a33d66aa21
xx008888 <xx008888@outlook.com>
2026-05-08 08:00:34 +0800

    add Intangible202605080000

---
+++ src/main/java/org/op/j2026/Intangible202605080000.java
@@ -0,0 +1,25 @@
+package org.op.j2026;
+
+public class Intangible202605080000 {
+
+    public String i0; 
+
+    public String n1; 
+
+    public String t2; 
+
+    public String a3; 
+
+    public String n4; 
+
+    public String g5; 
+
+    public String i6; 
+
+    public String b7; 
+
+    public String l8; 
+
+    public String e9; 
+
+}
