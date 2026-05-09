4dfb6cdf3afa428ea9274589eed6be5d4cb3220a
xx008888 <xx008888@outlook.com>
2026-05-10 00:00:31 +0800

    add Treasury202605091600

---
+++ src/main/java/org/op/j2026/Treasury202605091600.java
@@ -0,0 +1,21 @@
+package org.op.j2026;
+
+public class Treasury202605091600 {
+
+    public String t0; 
+
+    public String r1; 
+
+    public String e2; 
+
+    public String a3; 
+
+    public String s4; 
+
+    public String u5; 
+
+    public String r6; 
+
+    public String y7; 
+
+}
