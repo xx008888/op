167dc37b4b00d0bebbd93fc8a8caf867606d5baf
xx008888 <xx008888@outlook.com>
2026-05-09 08:00:03 +0800

    add Reader202605090000

---
+++ src/main/java/org/op/j2026/Reader202605090000.java
@@ -0,0 +1,17 @@
+package org.op.j2026;
+
+public class Reader202605090000 {
+
+    public String r0; 
+
+    public String e1; 
+
+    public String a2; 
+
+    public String d3; 
+
+    public String e4; 
+
+    public String r5; 
+
+}
