65292118e5b233bdd1dcba1c1823ea330a8f89e3
xx008888 <xx008888@outlook.com>
2026-05-12 08:00:36 +0800

    add Indict202605120000

---
+++ src/main/java/org/op/j2026/Indict202605120000.java
@@ -0,0 +1,17 @@
+package org.op.j2026;
+
+public class Indict202605120000 {
+
+    public String i0; 
+
+    public String n1; 
+
+    public String d2; 
+
+    public String i3; 
+
+    public String c4; 
+
+    public String t5; 
+
+}
