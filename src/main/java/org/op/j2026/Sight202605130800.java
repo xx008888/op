885354f87abf110a1560ff15b999218a5cbbf640
xx008888 <xx008888@outlook.com>
2026-05-13 16:00:57 +0800

    add Sight202605130800

---
+++ src/main/java/org/op/j2026/Sight202605130800.java
@@ -0,0 +1,15 @@
+package org.op.j2026;
+
+public class Sight202605130800 {
+
+    public String s0; 
+
+    public String i1; 
+
+    public String g2; 
+
+    public String h3; 
+
+    public String t4; 
+
+}
