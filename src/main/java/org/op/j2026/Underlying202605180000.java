883681fa3d26d31de9e4f34d88ad1493e552c972
xx008888 <xx008888@outlook.com>
2026-05-18 08:00:51 +0800

    add Underlying202605180000

---
+++ src/main/java/org/op/j2026/Underlying202605180000.java
@@ -0,0 +1,25 @@
+package org.op.j2026;
+
+public class Underlying202605180000 {
+
+    public String u0; 
+
+    public String n1; 
+
+    public String d2; 
+
+    public String e3; 
+
+    public String r4; 
+
+    public String l5; 
+
+    public String y6; 
+
+    public String i7; 
+
+    public String n8; 
+
+    public String g9; 
+
+}
