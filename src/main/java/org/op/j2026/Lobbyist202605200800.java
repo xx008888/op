9cfe32d11de5cb90b8fec2a37229e78c0631edee
xx008888 <xx008888@outlook.com>
2026-05-20 16:00:54 +0800

    add Lobbyist202605200800

---
+++ src/main/java/org/op/j2026/Lobbyist202605200800.java
@@ -0,0 +1,21 @@
+package org.op.j2026;
+
+public class Lobbyist202605200800 {
+
+    public String l0; 
+
+    public String o1; 
+
+    public String b2; 
+
+    public String b3; 
+
+    public String y4; 
+
+    public String i5; 
+
+    public String s6; 
+
+    public String t7; 
+
+}
