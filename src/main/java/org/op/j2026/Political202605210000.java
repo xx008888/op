875ba71d134a4cb8e2fd20922ae17d1bb49ee7e2
xx008888 <xx008888@outlook.com>
2026-05-21 08:00:54 +0800

    add Political202605210000

---
+++ src/main/java/org/op/j2026/Political202605210000.java
@@ -0,0 +1,23 @@
+package org.op.j2026;
+
+public class Political202605210000 {
+
+    public String p0; 
+
+    public String o1; 
+
+    public String l2; 
+
+    public String i3; 
+
+    public String t4; 
+
+    public String i5; 
+
+    public String c6; 
+
+    public String a7; 
+
+    public String l8; 
+
+}
