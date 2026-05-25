bc655a062b492c60101be347c2cd27dbc4565881
xx008888 <xx008888@outlook.com>
2026-05-25 08:00:44 +0800

    add Personal202605250000

---
+++ src/main/java/org/op/j2026/Personal202605250000.java
@@ -0,0 +1,21 @@
+package org.op.j2026;
+
+public class Personal202605250000 {
+
+    public String p0; 
+
+    public String e1; 
+
+    public String r2; 
+
+    public String s3; 
+
+    public String o4; 
+
+    public String n5; 
+
+    public String a6; 
+
+    public String l7; 
+
+}
