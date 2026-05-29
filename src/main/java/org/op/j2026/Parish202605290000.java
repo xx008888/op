933e53fce603905be960cb6b1965f29404f3d92e
xx008888 <xx008888@outlook.com>
2026-05-29 08:00:12 +0800

    add Parish202605290000

---
+++ src/main/java/org/op/j2026/Parish202605290000.java
@@ -0,0 +1,17 @@
+package org.op.j2026;
+
+public class Parish202605290000 {
+
+    public String p0; 
+
+    public String a1; 
+
+    public String r2; 
+
+    public String i3; 
+
+    public String s4; 
+
+    public String h5; 
+
+}
