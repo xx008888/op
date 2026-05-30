5b3ba9f170ce3e4099c25ddd04142b7d86628dab
xx008888 <xx008888@outlook.com>
2026-05-30 08:00:41 +0800

    add Tactic202605300000

---
+++ src/main/java/org/op/j2026/Tactic202605300000.java
@@ -0,0 +1,17 @@
+package org.op.j2026;
+
+public class Tactic202605300000 {
+
+    public String t0; 
+
+    public String a1; 
+
+    public String c2; 
+
+    public String t3; 
+
+    public String i4; 
+
+    public String c5; 
+
+}
