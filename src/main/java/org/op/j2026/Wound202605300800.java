b8c1f9da54102dcbfd6ba71d6ca3aedd2db2b0a3
xx008888 <xx008888@outlook.com>
2026-05-30 16:00:40 +0800

    add Wound202605300800

---
+++ src/main/java/org/op/j2026/Wound202605300800.java
@@ -0,0 +1,15 @@
+package org.op.j2026;
+
+public class Wound202605300800 {
+
+    public String w0; 
+
+    public String o1; 
+
+    public String u2; 
+
+    public String n3; 
+
+    public String d4; 
+
+}
