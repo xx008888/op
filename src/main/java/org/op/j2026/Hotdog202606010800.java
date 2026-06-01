e1a3f791058cfd3bb8bd10eff8011edf900f8a7a
xx008888 <xx008888@outlook.com>
2026-06-01 16:00:52 +0800

    add Hotdog202606010800

---
+++ src/main/java/org/op/j2026/Hotdog202606010800.java
@@ -0,0 +1,17 @@
+package org.op.j2026;
+
+public class Hotdog202606010800 {
+
+    public String h0; 
+
+    public String o1; 
+
+    public String t2; 
+
+    public String d3; 
+
+    public String o4; 
+
+    public String g5; 
+
+}
