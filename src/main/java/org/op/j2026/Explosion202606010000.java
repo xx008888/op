1817566680eed0a018e9e1bd9088b62aa30cc3f5
xx008888 <xx008888@outlook.com>
2026-06-01 08:00:45 +0800

    add Explosion202606010000

---
+++ src/main/java/org/op/j2026/Explosion202606010000.java
@@ -0,0 +1,23 @@
+package org.op.j2026;
+
+public class Explosion202606010000 {
+
+    public String e0; 
+
+    public String x1; 
+
+    public String p2; 
+
+    public String l3; 
+
+    public String o4; 
+
+    public String s5; 
+
+    public String i6; 
+
+    public String o7; 
+
+    public String n8; 
+
+}
