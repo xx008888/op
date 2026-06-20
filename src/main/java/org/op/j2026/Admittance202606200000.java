bddb2d67149036fc76ecda4c523982c920e29281
xx008888 <xx008888@outlook.com>
2026-06-20 08:00:27 +0800

    add Admittance202606200000

---
+++ src/main/java/org/op/j2026/Admittance202606200000.java
@@ -0,0 +1,25 @@
+package org.op.j2026;
+
+public class Admittance202606200000 {
+
+    public String a0; 
+
+    public String d1; 
+
+    public String m2; 
+
+    public String i3; 
+
+    public String t4; 
+
+    public String t5; 
+
+    public String a6; 
+
+    public String n7; 
+
+    public String c8; 
+
+    public String e9; 
+
+}
