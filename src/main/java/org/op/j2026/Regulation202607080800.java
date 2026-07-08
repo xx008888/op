57230baa2b8ef948570ed104abd7b9ca842e54b5
xx008888 <xx008888@outlook.com>
2026-07-08 16:00:26 +0800

    add Regulation202607080800

---
+++ src/main/java/org/op/j2026/Regulation202607080800.java
@@ -0,0 +1,25 @@
+package org.op.j2026;
+
+public class Regulation202607080800 {
+
+    public String r0; 
+
+    public String e1; 
+
+    public String g2; 
+
+    public String u3; 
+
+    public String l4; 
+
+    public String a5; 
+
+    public String t6; 
+
+    public String i7; 
+
+    public String o8; 
+
+    public String n9; 
+
+}
