55c0b6118cebf9cfe956ed4211558a2f31f28676
xx008888 <xx008888@outlook.com>
2026-07-20 16:00:56 +0800

    add Prescription202607200800

---
+++ src/main/java/org/op/j2026/Prescription202607200800.java
@@ -0,0 +1,29 @@
+package org.op.j2026;
+
+public class Prescription202607200800 {
+
+    public String p0; 
+
+    public String r1; 
+
+    public String e2; 
+
+    public String s3; 
+
+    public String c4; 
+
+    public String r5; 
+
+    public String i6; 
+
+    public String p7; 
+
+    public String t8; 
+
+    public String i9; 
+
+    public String o10; 
+
+    public String n11; 
+
+}
