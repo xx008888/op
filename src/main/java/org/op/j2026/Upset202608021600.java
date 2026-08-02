55a76499f8421a8c4a6cb5256913cd0c8b5621fe
xx008888 <xx008888@outlook.com>
2026-08-03 00:00:02 +0800

    add Upset202608021600

---
+++ src/main/java/org/op/j2026/Upset202608021600.java
@@ -0,0 +1,15 @@
+package org.op.j2026;
+
+public class Upset202608021600 {
+
+    public String u0; 
+
+    public String p1; 
+
+    public String s2; 
+
+    public String e3; 
+
+    public String t4; 
+
+}
