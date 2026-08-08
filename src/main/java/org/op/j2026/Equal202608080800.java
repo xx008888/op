31eb389590d0cdfdb204559193bf5c6f21c226d3
xx008888 <xx008888@outlook.com>
2026-08-08 16:00:17 +0800

    add Equal202608080800

---
+++ src/main/java/org/op/j2026/Equal202608080800.java
@@ -0,0 +1,15 @@
+package org.op.j2026;
+
+public class Equal202608080800 {
+
+    public String e0; 
+
+    public String q1; 
+
+    public String u2; 
+
+    public String a3; 
+
+    public String l4; 
+
+}
