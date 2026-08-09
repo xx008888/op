3b62fd4f38f18e9765b3549a1004210065c439aa
xx008888 <xx008888@outlook.com>
2026-08-09 08:00:30 +0800

    add Indoor202608090000

---
+++ src/main/java/org/op/j2026/Indoor202608090000.java
@@ -0,0 +1,17 @@
+package org.op.j2026;
+
+public class Indoor202608090000 {
+
+    public String i0; 
+
+    public String n1; 
+
+    public String d2; 
+
+    public String o3; 
+
+    public String o4; 
+
+    public String r5; 
+
+}
