35597d602495e80bb5f08079dbc419efb15475e2
xx008888 <xx008888@outlook.com>
2026-08-16 00:00:31 +0800

    add Charge202608151600

---
+++ src/main/java/org/op/j2026/Charge202608151600.java
@@ -0,0 +1,17 @@
+package org.op.j2026;
+
+public class Charge202608151600 {
+
+    public String c0; 
+
+    public String h1; 
+
+    public String a2; 
+
+    public String r3; 
+
+    public String g4; 
+
+    public String e5; 
+
+}
