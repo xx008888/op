7eec9200551e1083ccb9d4599dd09f10420bf238
xx008888 <xx008888@outlook.com>
2026-08-22 00:00:50 +0800

    add Occupancy202608211600

---
+++ src/main/java/org/op/j2026/Occupancy202608211600.java
@@ -0,0 +1,23 @@
+package org.op.j2026;
+
+public class Occupancy202608211600 {
+
+    public String o0; 
+
+    public String c1; 
+
+    public String c2; 
+
+    public String u3; 
+
+    public String p4; 
+
+    public String a5; 
+
+    public String n6; 
+
+    public String c7; 
+
+    public String y8; 
+
+}
