fc8e246578cd81789978148c58c4d70dc3ab704b
xx008888 <xx008888@outlook.com>
2026-08-26 08:00:49 +0800

    add Participate202608260000

---
+++ src/main/java/org/op/j2026/Participate202608260000.java
@@ -0,0 +1,27 @@
+package org.op.j2026;
+
+public class Participate202608260000 {
+
+    public String p0; 
+
+    public String a1; 
+
+    public String r2; 
+
+    public String t3; 
+
+    public String i4; 
+
+    public String c5; 
+
+    public String i6; 
+
+    public String p7; 
+
+    public String a8; 
+
+    public String t9; 
+
+    public String e10; 
+
+}
