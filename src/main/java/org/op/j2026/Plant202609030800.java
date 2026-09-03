16ff0fe5cf4e81a487b0d2119654561c5df66373
xx008888 <xx008888@outlook.com>
2026-09-03 16:00:35 +0800

    add Plant202609030800

---
+++ src/main/java/org/op/j2026/Plant202609030800.java
@@ -0,0 +1,15 @@
+package org.op.j2026;
+
+public class Plant202609030800 {
+
+    public String p0; 
+
+    public String l1; 
+
+    public String a2; 
+
+    public String n3; 
+
+    public String t4; 
+
+}
