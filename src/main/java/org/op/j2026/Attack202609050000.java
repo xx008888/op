3745893338b1acfe11bf96df1bb01a9fc523d5a8
xx008888 <xx008888@outlook.com>
2026-09-05 08:00:35 +0800

    add Attack202609050000

---
+++ src/main/java/org/op/j2026/Attack202609050000.java
@@ -0,0 +1,17 @@
+package org.op.j2026;
+
+public class Attack202609050000 {
+
+    public String a0; 
+
+    public String t1; 
+
+    public String t2; 
+
+    public String a3; 
+
+    public String c4; 
+
+    public String k5; 
+
+}
