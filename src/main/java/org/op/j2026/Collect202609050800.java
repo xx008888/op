b7a17ef7465e35558734f5af7f1bb8edb062aae2
xx008888 <xx008888@outlook.com>
2026-09-05 16:00:35 +0800

    add Collect202609050800

---
+++ src/main/java/org/op/j2026/Collect202609050800.java
@@ -0,0 +1,19 @@
+package org.op.j2026;
+
+public class Collect202609050800 {
+
+    public String c0; 
+
+    public String o1; 
+
+    public String l2; 
+
+    public String l3; 
+
+    public String e4; 
+
+    public String c5; 
+
+    public String t6; 
+
+}
