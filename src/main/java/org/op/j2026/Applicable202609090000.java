0d9ca8bde3fe8fe5fe71fd6be4f333d5c2d3da8f
xx008888 <xx008888@outlook.com>
2026-09-09 08:00:13 +0800

    add Applicable202609090000

---
+++ src/main/java/org/op/j2026/Applicable202609090000.java
@@ -0,0 +1,25 @@
+package org.op.j2026;
+
+public class Applicable202609090000 {
+
+    public String a0; 
+
+    public String p1; 
+
+    public String p2; 
+
+    public String l3; 
+
+    public String i4; 
+
+    public String c5; 
+
+    public String a6; 
+
+    public String b7; 
+
+    public String l8; 
+
+    public String e9; 
+
+}
