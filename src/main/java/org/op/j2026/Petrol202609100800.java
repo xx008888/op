e50ca73f46900de96543316cb812f00d6475e1de
xx008888 <xx008888@outlook.com>
2026-09-10 16:00:27 +0800

    add Petrol202609100800

---
+++ src/main/java/org/op/j2026/Petrol202609100800.java
@@ -0,0 +1,17 @@
+package org.op.j2026;
+
+public class Petrol202609100800 {
+
+    public String p0; 
+
+    public String e1; 
+
+    public String t2; 
+
+    public String r3; 
+
+    public String o4; 
+
+    public String l5; 
+
+}
