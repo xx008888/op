1bf65e0f3a3249a463d40e758051c20fe03ae1b9
xx008888 <xx008888@outlook.com>
2026-09-10 08:00:27 +0800

    add Dynasty202609100000

---
+++ src/main/java/org/op/j2026/Dynasty202609100000.java
@@ -0,0 +1,19 @@
+package org.op.j2026;
+
+public class Dynasty202609100000 {
+
+    public String d0; 
+
+    public String y1; 
+
+    public String n2; 
+
+    public String a3; 
+
+    public String s4; 
+
+    public String t5; 
+
+    public String y6; 
+
+}
