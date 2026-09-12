984bcf74f965c696272542119ca87b5bfc7971a2
xx008888 <xx008888@outlook.com>
2026-09-13 00:00:12 +0800

    add Winery202609121600

---
+++ src/main/java/org/op/j2026/Winery202609121600.java
@@ -0,0 +1,17 @@
+package org.op.j2026;
+
+public class Winery202609121600 {
+
+    public String w0; 
+
+    public String i1; 
+
+    public String n2; 
+
+    public String e3; 
+
+    public String r4; 
+
+    public String y5; 
+
+}
