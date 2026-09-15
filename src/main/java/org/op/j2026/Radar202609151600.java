ed1bd476867c99e2bd1e4ad3bb44202ab22839fd
xx008888 <xx008888@outlook.com>
2026-09-16 00:00:13 +0800

    add Radar202609151600

---
+++ src/main/java/org/op/j2026/Radar202609151600.java
@@ -0,0 +1,15 @@
+package org.op.j2026;
+
+public class Radar202609151600 {
+
+    public String r0; 
+
+    public String a1; 
+
+    public String d2; 
+
+    public String a3; 
+
+    public String r4; 
+
+}
