c60e0ee9e579f286320f2afea655525ba8f8ef38
xx008888 <xx008888@outlook.com>
2026-09-26 08:00:12 +0800

    add Custody202609260000

---
+++ src/main/java/org/op/j2026/Custody202609260000.java
@@ -0,0 +1,19 @@
+package org.op.j2026;
+
+public class Custody202609260000 {
+
+    public String c0; 
+
+    public String u1; 
+
+    public String s2; 
+
+    public String t3; 
+
+    public String o4; 
+
+    public String d5; 
+
+    public String y6; 
+
+}
